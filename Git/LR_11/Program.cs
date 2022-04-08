ff53a40f39fdc574a24cc72519b57281181ebf88
Ybuvashka <77838592+Ybuvashka@users.noreply.github.com>
2022-04-08 22:49:35 +0300

    Add files via upload

---
+++ Git/LR_11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace LR_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "store.txt";
+            StreamReader reader = new StreamReader(path);
+            if (reader.EndOfStream)
+            {
+                Console.WriteLine("Файл пустий");
+            }
+            else
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            reader.Close();
+
+            StreamWriter writer = new StreamWriter(path);
+            writer.WriteLine(DateTime.Now.ToString());
+            writer.Close();
+
+        }
+    }
+}
